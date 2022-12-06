a9a968eeff219dceb28ccbe8e44dbfe97c9e00fe
wonchenqin <23247166@student.uwa.edu.au>
2022-12-06 14:09:41 +0800

    Refactor game init function
    Moved deck related functions to deal() and included all necessary statements to init_game()

--- Tycoon/game.py
+++ Tycoon/game.py
@@ -11,7 +11,6 @@ import typing
 class Game:
     def __init__(self, playerlist: typing.List[Player]):
         self.deck = Deck()
-        self.deck.shuffle()
         self.data = {
             "is_start_of_round": True,
             "play_to_beat": [],
@@ -26,20 +25,23 @@ class Game:
         self.game_end = False
         self.last_round_victor = None
 
-        self.playerlist = playerlist    # may have more than 4 players
-        self.data["hand_sizes"] = [0] * len(playerlist)
-        self.data["scores"] = [0] * len(playerlist)
+        self.playerlist = playerlist    # May have more than 4 players
+        self.playerlist_orig = None
 
     def deal(self):
-        # empty all hands
+        # Reset and shuffle the deck
+        self.deck.reset()
+        self.deck.shuffle()
+        
+        # Empty all hands
         for idx, player in enumerate(self.playerlist):
             player.hand = []
             self.data["hand_sizes"][idx] = 0
 
-        # randomise starting player
+        # Randomise starting player
         offset = random.randint(0, len(self.playerlist) - 1)
 
-        # deal cards to each player until deck is empty; deck size is fixed at 54
+        # Deal cards to each player until deck is empty; deck size is fixed at 54
         for i in range(54):
             card = self.deck.deal()
             curr_player = (i + offset) % len(self.playerlist)
@@ -48,7 +50,7 @@ class Game:
 
     def init_game(self):
         self.deal()
-        # player that has 3D will always go first
+        # Player that has 3D will always go first
         otherplayers = []
         firstplayer = None
 
@@ -58,9 +60,29 @@ class Game:
             else:
                 otherplayers.append(player)
 
-        # randomise the order of the other players
+        # Randomise the order of the other players
         random.shuffle(otherplayers)
         self.playerlist = [firstplayer] + otherplayers
+        
+        # Store original player order
+        self.playerlist_orig = [p.name for p in self.playerlist]
+        
+        # Resetting all game data
+        self.data = {
+            "is_start_of_round": True,
+            "play_to_beat": [],
+            "round_history": [[]],
+            "hand_sizes": [],
+            "scores": [],
+            "round_no": 0,
+            "is_rev": False,
+        }
+        self.trick_end = False
+        self.round_end = False
+        self.game_end = False
+        self.last_round_victor = None
+        self.data["hand_sizes"] = [0] * len(self.playerlist)
+        self.data["scores"]     = [0] * len(self.playerlist)
 
     def play_text_based(self):
         pass
